a2663812cd1968e6d46b30fbff28dff9cb34bc66
Ramon Mena <rmena@Ramons-MacBook-Pro.local>
2021-06-13 12:19:00 -0400

    Removing some console logs

--- src/redux/reducers/userReducer.ts
+++ src/redux/reducers/userReducer.ts
@@ -10,8 +10,6 @@ export default function userReducer(
   state: UserState = { name: "" },
   action: UserActionReducerProps
 ) {
-    console.log("Action", action.type, action.payload);
-    console.log("Action", action);
   switch (action.type) {
     case UserTypes.SET_USER:
       return { ...state, name: action.payload };
